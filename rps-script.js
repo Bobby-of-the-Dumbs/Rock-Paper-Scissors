f60c8cdb1a7208df131190f481e97e48562a6272
Bobby-of-the-Dumbs <georgetombob@gmail.com>
2026-05-09 23:58:11 -0500

    rps-script.js

---
+++ rps-script.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    if (Math.random() % 2 === 0) {
+        return "Rock"
+
+    }
+    else if (Math.random() % 3 || 5 === 0) {
+       return "Scissors" 
+    }
+    else {
+        return "Paper"
+    }
+    
+    
+}
+
